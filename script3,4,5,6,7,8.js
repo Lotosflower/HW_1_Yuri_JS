4753ad2ce0d2a1f6418bcfcd5ce3a2e007f5505f
Yuri <korytko_88@mail.ru>
2021-11-29 14:24:49 +0300

    HW_1_Yuri_JS

---
+++ script3,4,5,6,7,8.js
@@ -0,0 +1,49 @@
+
+// Задание 3
+
+let heigh = 23,
+    width = 10;
+    Spryam = heigh * width
+console.log(Spryam)
+
+  
+        // Задание 5
+    let rKruga = 5,
+
+    SKruga = Math.PI * rKruga ** 2
+    console.log(SKruga)
+
+
+   // Задание 6
+   let a = 5,
+       b = 7,
+       hTrap = 10;
+
+   STrap = ((a + b) / 2) * hTrap
+   console.log(STrap)
+
+
+   // Задание 7
+   let S = 2000000
+       pGodovaya = 10,
+       years = 5;
+
+   Pereplata = ((S * pGodovaya) * 5) / 100
+   console.log(Pereplata)
+
+
+   // Задание 8
+    // c + 2(x - b) = 16
+    let c = 8
+    b = 3  
+
+    x = (16 + 2 * b - c) / 2
+    console.log(x)
+
+    // b(x + 15) = c + 6x
+    x = - (8 / 3) + 15
+    console.log(x)
+
+    //x+2x+ax+bx=23780. 
+    x = 23780 / 14
+    console.log(x)
